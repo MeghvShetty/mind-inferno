2fe9bb999ab72d84ae6f570d75ce80a25f405857
Megh Shetty <meghvshetty@outlook.com>
2025-08-13 21:27:31 +0100

    js

---
+++ javascript/the-odin-project/my-js.js
@@ -0,0 +1,18 @@
+let name = "John";
+let surname = "Dever";
+
+console.log(name);
+console.log(surname);
+
+name = "Neil";
+surname = "Young"
+
+console.log(name);
+console.log(surname);
+
+const pi = 3.14;
+
+
+console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1))
